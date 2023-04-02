fc70a5b4c4cdaa2dc1b2a56418cea69157dddc15
Soraran6 <shiki1893@gmail.com>
2023-04-02 20:29:16 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = ["the dog", "my granma", "his turtle", "my bird"];
+let what = ["ate", "pissed", "crushed", "broke"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function generateExcuse() {
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+  let excuse =
+    "Sorry, I couldn't do it because " +
+    randomWho +
+    " " +
+    randomWhat +
+    " " +
+    "my work " +
+    randomWhen +
+    ".";
+  document.getElementById("excuse").innerHTML = excuse;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  const generateBtn = document.getElementById("generate-btn");
+  generateBtn.addEventListener("click", generateExcuse);
+});
